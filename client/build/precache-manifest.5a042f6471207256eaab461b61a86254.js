b8b4a066b837c439fe317f7468268e019db6b854
Michael Jeffcoat <38878555+mikegjeff@users.noreply.github.com>
2020-04-13 15:49:04 +1200

    updated dependencies

---
+++ client/build/precache-manifest.5a042f6471207256eaab461b61a86254.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "99c4e4242f9269f4c8a4",
+    "url": "/static/js/main.99c4e424.chunk.js"
+  },
+  {
+    "revision": "a80e7c2f981f1feeb1e2",
+    "url": "/static/js/1.a80e7c2f.chunk.js"
+  },
+  {
+    "revision": "99c4e4242f9269f4c8a4",
+    "url": "/static/css/main.558b72a9.chunk.css"
+  },
+  {
+    "revision": "0d9b12f6b588fabb93373a5c56e99cc4",
+    "url": "/index.html"
+  }
+];
