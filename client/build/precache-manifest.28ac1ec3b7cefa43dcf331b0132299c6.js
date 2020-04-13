e0725fd36b942197818e58597dc7c45ed30c1d5a
Michael Jeffcoat <38878555+mikegjeff@users.noreply.github.com>
2020-04-13 15:17:24 +1200

    Initial commit

---
+++ client/build/precache-manifest.28ac1ec3b7cefa43dcf331b0132299c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2c31596e7add9d1d8db4",
+    "url": "/static/js/main.2c31596e.chunk.js"
+  },
+  {
+    "revision": "a49092d874b05a6ee9a4",
+    "url": "/static/js/1.a49092d8.chunk.js"
+  },
+  {
+    "revision": "2c31596e7add9d1d8db4",
+    "url": "/static/css/main.8330a11f.chunk.css"
+  },
+  {
+    "revision": "303e39331ca23ec32f72dbd76b1212f1",
+    "url": "/index.html"
+  }
+];
